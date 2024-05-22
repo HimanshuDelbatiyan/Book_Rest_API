c765243196831c48ea2e48e5c3e2819ca08533ec
HimanshuDelbatiyan <HimanshuDelbatiyan@gmail.com>
2024-05-22 14:07:41 -0400

    Delete a Book Updated

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook ,updateBook, listBooks, getSingleBook} from "./bookControllers";
+import { createBook ,updateBook, listBooks, getSingleBook, deleteBook} from "./bookControllers";
 import multer from "multer";
 import authenticate from "../middleware/authenticate";
 import path from "path";
@@ -28,6 +28,9 @@ bookRouter.patch("/:bookId",authenticate, upload.fields([
 bookRouter.get("/",listBooks)
 
 bookRouter.get("/:bookId",getSingleBook)
+
+bookRouter.delete("/:bookId",authenticate, deleteBook)
+
 
 
 export default bookRouter;
